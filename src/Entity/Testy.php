01dfd2d187a3c01241a7cfa3f5cdc5f2600d9c8d
Tomas Jucius <jucius.tomas@gmail.com>
2023-03-11 21:39:58 +0200

    MPB-1 api, doctrine installed

---
+++ src/Entity/Testy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\TestyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestyRepository::class)]
+#[ApiResource]
+class Testy
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $score = null;
+
+    #[ORM\Column]
+    private ?bool $super = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function isSuper(): ?bool
+    {
+        return $this->super;
+    }
+
+    public function setSuper(bool $super): self
+    {
+        $this->super = $super;
+
+        return $this;
+    }
+}
